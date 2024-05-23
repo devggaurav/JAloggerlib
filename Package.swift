b7872885b485ccbe6c47ea438864dec4f862c21e
Gaurav <gaurav.chauhan@justanswer.com>
2024-05-23 16:30:46 +0530

    fixed untracked files

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "jaLogger",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        .library(
+            name: "jaLogger",
+            targets: ["jaLogger"]
+        )
+    ],
+    targets: [
+        .binaryTarget(
+            name: "jaLogger",
+            url: "https://github.com/devggaurav/JAloggerlib/blob/main/framworkZip/jaLogger.xcframework.zip",
+            checksum: "abc607dbf2dee0b97b10dfbaad012edb057506ec6a2dbd6381b094d2e777f917"
+        )
+    ]
+)
